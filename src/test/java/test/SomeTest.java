2d145af8bf548164dbd1eaf961bb6b41fd2d21a7
shiry <2427718792@qq.com>
2025-12-13 11:22:49 +0800

    init java

--- src/test/java/test/SomeTest.java
+++ src/test/java/test/SomeTest.java
@@ -36,20 +36,20 @@ public class SomeTest extends Base {
         run();
     }
 
-    @Test public void test3() {
-        run();
-    }
-
-    @Test public void test4() {
-        run();
-    }
-
-    @Test public void test5() {
-        run();
-    }
-
-    @Test public void test6() {
-        run();
-    }
+//    @Test public void test3() {
+//        run();
+//    }
+//
+//    @Test public void test4() {
+//        run();
+//    }
+//
+//    @Test public void test5() {
+//        run();
+//    }
+//
+//    @Test public void test6() {
+//        run();
+//    }
 
 }
